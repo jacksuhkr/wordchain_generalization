894490f08e0bebcfc902c449610af98f908632d8
jacksuhkr <jacksuhkr@gmail.com>
2016-08-18 17:26:18 +0900

    1판씩 돌려봄

--- 끝말잇기 일반화/src/data_base/Words.java
+++ 끝말잇기 일반화/src/data_base/Words.java
@@ -2,8 +2,7 @@ package data_base;
 
 public class Words {
 //	private static String wordFile = "data_files/words.txt";
-//	private static String wordFile = "data_files/normal_words.txt";
-	private static String wordFile = "data_files/korean_normal_words.txt";
+	private static String wordFile = "data_files/normal_words.txt";
 	public static final int NUM_OF_WORDS=FileReader.getNumOfWords(wordFile);
 	public static final String[] words = new String[NUM_OF_WORDS];		
 	
